2bd54d5caaf042e8a2477f4d82deab3ca91e0e83
Eli Burch <komodo0619@gmail.com>
2020-10-15 00:10:27 -0700

    Fixed messy output

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ dictionary = {
 	b'cat': "an animal with ears",
 	b'lizard': " an animal without ears"
 }
-sock = socket.create_server(('192.168.254.15', 6789))
+sock = socket.create_server(('192.168.0.16', 6789))
 sock.listen()
 conn = sock.accept()[0]
-conn.send(b'Hello Eli')
-conn.send(b'Look up word: ')
-reply = conn.recv(4096)
-word = dictionary[reply]
-conn.send(str.encode(word))
-input()
+conn.send(b'Hello Eli\r\n')
+reply = b'none'
+while(reply != b'q'):
+	conn.send(b'Look up word: ')
+	reply = conn.recv(4096)
+	try:
+		word = dictionary[reply]
+		conn.send(str.encode(word) + b"\r\n")
+	except:
+		if(reply != b'q'):
+			conn.send(b"ERROR word not found\r\n")
+	conn.recv(4096)
 conn.close()
