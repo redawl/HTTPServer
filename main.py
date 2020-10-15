79aeda8939a4e1f3cd826f8a78bb428abab6dc6a
redawl <komodo0619@gmail.com>
2020-10-14 20:10:38 -0700

    On Chapter 6 currently

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello World!')
+import socket
+dictionary = {
+	b'cat': "an animal with ears",
+	b'lizard': " an animal without ears"
+}
+sock = socket.create_server(('192.168.254.15', 6789))
+sock.listen()
+conn = sock.accept()[0]
+conn.send(b'Hello Eli')
+conn.send(b'Look up word: ')
+reply = conn.recv(4096)
+word = dictionary[reply]
+conn.send(str.encode(word))
+input()
+conn.close()
