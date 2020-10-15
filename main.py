18e0008f163efd48e62f50748a6f4a90868fb7ce
Eli Burch <komodo0619@gmail.com>
2020-10-15 11:39:06 -0700

    Finished Chapter 9

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
+#includes
 import socket
+import threading
+
+#This function will be run for every client that connects
+def for_each_client(sock, conn, dictionary):
+	conn.send(b'Hello Eli\r\n')
+	reply = b'none'
+	while(reply != b'q'):
+		conn.send(b'Look up word: ')
+		reply = conn.recv(4096)
+		try:
+			word = dictionary[reply]
+			conn.send(str.encode(word) + b"\r\n")
+		except:
+			if(reply != b'q'):
+				conn.send(b"ERROR cant find " + reply + b"\r\n")
+		conn.recv(4096)
+	conn.close()
+
+#This will hold all the dictionary definitions
 dictionary = {
-	b'cat': "an animal with ears",
-	b'lizard': " an animal without ears"
+	b'GET cat': "ANSWER An animal with ears",
+	b'GET lizard': "ANSWER An animal without ears"
 }
+
+#This block of code handles the new clients and passes control to for_each_client()
 sock = socket.create_server(('192.168.0.16', 6789))
-sock.listen()
-conn = sock.accept()[0]
-conn.send(b'Hello Eli\r\n')
-reply = b'none'
-while(reply != b'q'):
-	conn.send(b'Look up word: ')
-	reply = conn.recv(4096)
-	try:
-		word = dictionary[reply]
-		conn.send(str.encode(word) + b"\r\n")
-	except:
-		if(reply != b'q'):
-			conn.send(b"ERROR word not found\r\n")
-	conn.recv(4096)
-conn.close()
+while(1):
+	sock.listen(10)
+	conn = sock.accept()[0]
+	new_thread = threading.Thread(None, for_each_client, None, (sock, conn, dictionary))
+	new_thread.start()
