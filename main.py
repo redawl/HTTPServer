d176b8d6a7d601fe21fd5cf9fcb73d36a67a7bbe
Eli Burch <komodo0619@gmail.com>
2020-10-15 13:38:10 -0700

    Finished Chapter 12

--- main.py
+++ main.py
@@ -4,19 +4,30 @@ import threading
 
 #This function will be run for every client that connects
 def for_each_client(sock, conn, dictionary):
-	conn.send(b'Hello Eli\r\n')
 	reply = b'none'
-	while(reply != b'q'):
-		conn.send(b'Look up word: ')
-		reply = conn.recv(4096)
-		try:
-			word = dictionary[reply]
-			conn.send(str.encode(word) + b"\r\n")
-		except:
-			if(reply != b'q'):
-				conn.send(b"ERROR cant find " + reply + b"\r\n")
-		conn.recv(4096)
+	reply = conn.recv(4096)
+	reply_raw = str(reply)[2:-1]
+	requests = reply_raw.split("\\r\\n")
+	reply_parsed = requests[0].split(" ")
+	print('METHOD: ' + reply_parsed[0] + '\n')
+	print('RESOURCE: ' + reply_parsed[1] + '\n')
+	print('VERSION: ' + reply_parsed[2] + '\n')
+	for i in range(len(requests) - 1):
+		print(requests[i + 1])
+
+	try:
+		file = open("./www" + reply_parsed[1], "r")
+		conn.send(reply_parsed[2].encode() + b"200" + b"OK")
+		conn.send(b"\r\n")
+		conn.send(file.read().encode())
+	except:
+		print('Requested File Not Found')
+		conn.send(reply_parsed[2].encode() + b"404" + b"Not Found")
+		conn.send(b"\r\n")
+		conn.send(b"Sorry we don't have that file!")
+
 	conn.close()
+	print('Client Disconnected')
 
 #This will hold all the dictionary definitions
 dictionary = {
@@ -29,5 +40,6 @@ sock = socket.create_server(('192.168.0.16', 6789))
 while(1):
 	sock.listen(10)
 	conn = sock.accept()[0]
+	print('Client Connected')
 	new_thread = threading.Thread(None, for_each_client, None, (sock, conn, dictionary))
 	new_thread.start()
