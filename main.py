2ac31380f2bc3ec80015f774983c8956c86bdb82
Eli Burch <komodo0619@gmail.com>
2020-10-18 06:26:23 -0700

    Fixed issue with sending images

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ def get_file_type(file):
 		ret = "image/x-icon"
 	elif(extension == "css"):
 		ret = "text/css; charset=UTF-8"
+	elif(extension == "jpg"):
+		ret = "image/jpeg"
 	else:
 		ret = "text; charset=UTF-8"
 
 	return ret
 
-#This function makes sure the request stays in the bounds of the root directory
+#This function makes sure the request stays in the bounds of the web directory
 def check_if_forbidden(file):
 	updir = len(file.split(".."))
 	downdir = len(file.split("/"))
-	if(downdir / updir < 2):
+	if(updir == 0 or downdir / updir < 2):
 		raise ValueError()
 
 #This function will be run for every client that connects
@@ -36,18 +38,18 @@ def for_each_client(sock, conn, web_directory):
 	HTTP_VERSION = reply_parsed[2]
 	print("METHOD: " + METHOD + '\n')
 	try:
-		check_if_forbidden(RESOURCE)
+		check_if_forbidden(RESOURCE)#Throws ValueError if resorce path is outside of web_directory
 		if(RESOURCE != "/"):
 			print(web_directory + RESOURCE)
 		else:
 			RESOURCE = "/index.html"
 
-		file = open(web_directory + RESOURCE, "r")
+		file = open(web_directory + RESOURCE, "rb")#Throws IOError if file doesn't exist
 		extension = get_file_type(RESOURCE)
 		conn.send(HTTP_VERSION.encode() + b" 200 " + b"OK\r\n")
 		conn.send(b"Content-Type: " + extension.encode() + b"\r\n")
 		conn.send(b"\r\n")
-		conn.send(file.read().encode())
+		conn.send(file.read())
 	except IOError:
 		conn.send(HTTP_VERSION.encode() + b" 404 " + b"Not Found")
 		conn.send(b"\r\n")
