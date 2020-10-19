c8080344b591c2ab81e907df0a1740295631ebe2
Eli Burch <komodo0619@gmail.com>
2020-10-18 18:47:38 -0700

    Added basic testing function - Still learning how to correctly implement testing and am not sure if I'm doing it right

---
+++ tests.py
@@ -0,0 +1,15 @@
+import socket
+
+def test_outside_dir(client):
+	client.send(b"GET /../README.md HTTP/1.1\r\n")
+	assert(client.recv(4096) == b'HTTP/1.1 403 Forbidden'), "Client gained access to private info!"
+	client.detach()
+	client = socket.socket()
+	client.connect(("192.168.0.16", 6789))
+	client.send(b"GET /www/ww/../w/../../../README.md HTTP/1.1\r\n")
+	assert(client.recv(4096) == b'HTTP/1.1 403 Forbidden'), "Client gained access to private info!"
+
+def this_file_doesnt_exist(client)
+client = socket.socket()
+client.connect(("192.168.0.16", 6789))
+test_outside_dir(client)
