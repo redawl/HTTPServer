968d5355660bf8c66c74ae858b0bb2c54c043d48
Eli Burch <komodo0619@gmail.com>
2020-10-24 11:01:28 -0700

    Changed variables to lowercase, and added if statement to server.py so that server wont run if imported

--- server.py
+++ server.py
@@ -4,19 +4,19 @@ import threading
 from server_functions import *
 
 ## MAIN FUNCTION ##
+if __name__ == "__main__":
+	#Read Configuration from config.txt
+	cfg = read_config()
+	host = cfg[0]
+	port = cfg[1]
+	web_directory = cfg[2]
 
-#Read Configuration from config.txt
-cfg = read_config()
-host = cfg[0]
-port = cfg[1]
-web_directory = cfg[2]
-
-#This block of code handles the new clients and passes control to for_each_client()
-sock = socket.create_server((host, int(port)))
-print("Creating Server on " + host + ":" + port + '\r\n')
-while(1):
-	sock.listen(10)
-	conn = sock.accept()[0]
-	print('Client Connected')
-	new_thread = threading.Thread(None, for_each_client, None, (sock, conn, web_directory))
-	new_thread.start()
+	#This block of code handles the new clients and passes control to for_each_client()
+	sock = socket.create_server((host, int(port)))
+	print("Creating Server on " + host + ":" + port + '\r\n')
+	while True:
+		sock.listen(10)
+		conn = sock.accept()[0]
+		print('Client Connected')
+		new_thread = threading.Thread(None, for_each_client, None, (sock, conn, web_directory))
+		new_thread.start()
--- server_functions.py
+++ server_functions.py
@@ -29,32 +29,33 @@ def for_each_client(sock, conn, web_directory):
 	reply_raw = str(conn.recv(4096))[2:-1]
 	requests = reply_raw.split("\\r\\n")
 	reply_parsed = requests[0].split(" ")
-	METHOD = reply_parsed[0]
-	RESOURCE = reply_parsed[1]
-	HTTP_VERSION = reply_parsed[2]
-	print("METHOD: " + METHOD + '\n')
+	method = reply_parsed[0]
+	resource = reply_parsed[1]
+	http_version = reply_parsed[2]
+	print("METHOD: " + method + '\n')
 	try:
-		check_if_forbidden(RESOURCE)#Throws ValueError if resorce path is outside of web_directory
-		if(RESOURCE != "/"):
-			print(web_directory + RESOURCE)
+		check_if_forbidden(resource)#Throws ValueError if resorce path is outside of web_directory
+		if(resource != "/"):
+			print(web_directory + resource)
 		else:
-			RESOURCE = "/index.html"
+			resource = "/index.html"
 
-		file = open(web_directory + RESOURCE, "rb")#Throws IOError if file doesn't exist
-		extension = get_file_type(RESOURCE)
-		conn.send(HTTP_VERSION.encode() + b" 200 " + b"OK\r\n")
+		file = open(web_directory + resource, "rb")#Throws IOError if file doesn't exist
+		extension = get_file_type(resource)
+		conn.send(http_version.encode() + b" 200 " + b"OK\r\n")
 		conn.send(b"Content-Type: " + extension.encode() + b"\r\n")
 		conn.send(b"\r\n")
 		conn.send(file.read())
 	except IOError:
-		conn.send(HTTP_VERSION.encode() + b" 404 " + b"Not Found")
+		conn.send(http_version.encode() + b" 404 " + b"Not Found")
 		conn.send(b"\r\n")
 		conn.send(b"Sorry we don't have that file!")
 	except ValueError:
-		conn.send(HTTP_VERSION.encode() + b" 403 " + b"Forbidden")
+		conn.send(http_version.encode() + b" 403 " + b"Forbidden")
 		conn.send(b"\r\n")
-		conn.send(RESOURCE.encode() + b" is forbidden!")
+		conn.send(resource.encode() + b" is forbidden!")
 
+	file.close()
 	conn.close()
 	print('Client Disconnected')
 
